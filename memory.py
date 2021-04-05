ce7a2a98f23fabce3dedf2684e297f8af4fd50b2
aditiaggarwal28 <2019csb1063@iitrpr.ac.in>
2021-04-05 22:53:51 +0530

    memory file

---
+++ memory.py
@@ -0,0 +1,6 @@
+def memory(return_address,rz,l,rm):
+    if(l[0]=='R' or l[i]=='I'):
+        return rz
+    elif(l[0]=='S'):
+        #we need one structure for memory where we store memory
+        
