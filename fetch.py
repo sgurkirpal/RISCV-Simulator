b4db995f3eb6d780e37ce24192f578a364897449
harshest2019 <harshp19012002@gmail.com>
2021-04-06 16:19:26 +0530

    Edits to fetch.py
    Converting fetch to fucntion

--- fetch.py
+++ fetch.py
@@ -1,5 +1,4 @@
 #Returns two dictionaries one with the text and other with data part
-mc_file=open('Samplemc.txt')  #here sample file is mc file
 
 dict_text=dict()
 dict_data=dict()
@@ -18,10 +17,8 @@ def form_dict_data(sample_file):
         dict_data[words[0]]=words[1]
     return dict_data
 
-def fetch(mc_file):
+def fetch_file(mc_file):
     dict_text=form_dict_text(mc_file)
     dict_data=form_dict_data(mc_file)
 
     return dict_text,dict_data
-
-print(fetch(mc_file))
