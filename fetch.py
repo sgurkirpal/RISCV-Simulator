d7d93898af3f3735d37fa74ef232d45a0b61ec98
Kashishjan <77626027+Kashishjan@users.noreply.github.com>
2021-04-06 14:11:48 +0530

    Add files via upload

---
+++ fetch.py
@@ -0,0 +1,26 @@
+mc_file=open('Samplemc.txt')
+
+dict_text=dict()
+dict_data=dict()
+
+def form_dict_text(sample_file):
+    for line in sample_file:
+        words=line.split()
+        if words[0]=='0xffffc':
+            break
+        dict_text[words[0]]=words[1]
+    return dict_text
+
+def form_dict_data(sample_file):
+    for line in sample_file:
+        words=line.split()
+        dict_data[words[0]]=words[1]
+    return dict_data
+
+def fetch(mc_file):
+    dict_text=form_dict_text(mc_file)
+    dict_data=form_dict_data(mc_file)
+
+    return dict_text,dict_data
+
+print(fetch(mc_file))
