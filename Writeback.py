93e76e354e3fbb9873a335c831873ed6fc369a8d
aditiaggarwal28 <2019csb1063@iitrpr.ac.in>
2021-04-08 10:39:19 +0530

    debugging part 2

--- Writeback.py
+++ Writeback.py
@@ -1,9 +1,9 @@
 def write_back(muxy,l,register_dictionary):
     if(l[0]=='R' or l[0]=='I' or l[0]=='U' or l[0]=='UJ'):
-        muxy=muxy[2:]
-        muxy+='0'*(8-len(muxy))
-        muxy=muxy[6:8]+muxy[4:6]+muxy[2:4]+muxy[:2]
-        register_dictionary[int(l[len(l)-1],2)]='0x'+muxy
+        #muxy=muxy[2:]
+        #muxy+='0'*(8-len(muxy))
+        #muxy=muxy[6:8]+muxy[4:6]+muxy[2:4]+muxy[:2]
+        register_dictionary[int(l[len(l)-1],2)]=muxy
         return register_dictionary
     else:
         return register_dictionary
