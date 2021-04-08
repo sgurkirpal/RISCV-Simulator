240d9f1da1098611d1cbdc81ca0c20ef980ca157
aditiaggarwal28 <2019csb1063@iitrpr.ac.in>
2021-04-08 10:27:16 +0530

    Debugging part 1

--- Writeback.py
+++ Writeback.py
@@ -1,8 +1,9 @@
 def write_back(muxy,l,register_dictionary):
-    if(l[0]=='R' or l[0]=='S' or l[0]=='U' or l[0]=='UJ'):
+    if(l[0]=='R' or l[0]=='I' or l[0]=='U' or l[0]=='UJ'):
+        muxy=muxy[2:]
         muxy+='0'*(8-len(muxy))
         muxy=muxy[6:8]+muxy[4:6]+muxy[2:4]+muxy[:2]
-        register_dictionary[l[len(l)-1]]=muxy
+        register_dictionary[int(l[len(l)-1],2)]='0x'+muxy
         return register_dictionary
     else:
         return register_dictionary
--- memory.py
+++ memory.py
@@ -3,31 +3,34 @@
 def memory(return_address,rz,l,rm,memory_dictionary,pc_temp):
     if(l[1]=='lw'):
         sam=memory_dictionary[rz]
+        sam=sam[2:]
         am=sam[6:8]+sam[4:6]+sam[2:4]+sam[:2]
-        return am
+        return am,memory_dictionary
     elif(l[1]=='lb'):
         a=memory_dictionary[rz]
-        return '000000'+a[:2]
+        a=a[2:]
+        return '000000'+a[:2],memory_dictionary
     elif(l[1]=='lh'):
         a=memory_dictionary[rz]
-        return '0000'+a[:4]
+        a=a[2:]
+        return '0000'+a[:4],memory_dictionary
     elif(l[1]=='jalr'):
-        return pc_temp
+        return pc_temp,memory_dictionary
     elif(l[1]=='sw'):
         rm=str(rm)
         am=rm[6:8]+rm[4:6]+rm[2:4]+rm[:2]
-        memory_dictionary[rz]=am
-        return rz
+        memory_dictionary[rz]='0x'+am
+        return rz,memory_dictionary
     elif(l[1]=='sh'):
         rm=str(rm)
         am=rm[6:8]+rm[4:6]
-        memory_dictionary[rz]=am+'0000'
-        return rz
+        memory_dictionary[rz]='0x'+am+'0000'
+        return rz,memory_dictionary
     elif(l[1]=='sb'):
         rm=str(rm)
         am=rm[6:8]
-        memory_dictionary[rz]=am+'000000'
-        return rz
+        memory_dictionary[rz]='0x'+am+'000000'
+        return rz,memory_dictionary
     else:
-        return rz
+        return rz,memory_dictionary
     
