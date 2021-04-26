fa4bb9b85a858042bfc116c27e6b1d01a2674248
Gurkirpal Singh <sgurkirpal88@gmail.com>
2021-04-26 15:17:49 +0530

    Added files

--- src/priyanshu_gps_main.py
+++ src/priyanshu_gps_main.py
@@ -10,9 +10,13 @@ instruction_dict={}    #dictionary storing instructions
 data_dict={}    #dictionary storing data in memory
 
 reg={} #registers
+write_df_reg={}
+val_df_reg={}
+mem_df_reg={}
 reg[0]='0x0'
 for i in range(32):
     reg[i]='0x00000000'
+    write_df_reg[i] = 0
     if i==2:
         reg[i]='0x7FFFFFF0'
     if i==3:
@@ -55,12 +59,24 @@ while(1):
         write_pc.pop(0)
         #print("write",this_pc)
         if('rd' in decoded_info[this_pc]):
+            x=int(decoded_info[this_pc]['rd'], 2)
+            write_df_reg[x]=0  # as now its use has ended
             if(int(decoded_info[this_pc]['rd'],2)!=0):
                 reg,temp_string_writeback=Writeback.write_back(muxy,[decoded_info[this_pc]['type'],decoded_info[this_pc]['opr'],decoded_info[this_pc]['rd']],reg)
 
     #memory
     if len(mem_pc)!=0:
         this_pc=mem_pc[0]
+        rs1=decoded_info[this_pc].get('rs1','-1')
+        rs2=decoded_info[this_pc].get('rs2','-1')
+        if(rs1!='-1'):
+            x=int(rs1,2)
+            if(write_df_reg[x]==1):#that means data forwarding is required
+                reg[x]=mem_df_reg[x] # i add the value stored in the buffer
+        if(rs2!='-1'):
+            x=int(rs2,2)
+            if(write_df_reg[x]==1):#that means data forwarding is required
+                reg[x]=mem_df_reg[x] # i add the value stored in the buffer   
         mem_pc.pop(0)
         write_pc.append(this_pc)
         #print("mem",this_pc)
@@ -76,6 +92,14 @@ while(1):
             if(len(rz)!=10):
                 rz=rz[:2]+'0'*(10-len(rz))+rz[2:]
         #print(rz)
+        opr=decoded_info[this_pc].get('opr','-1')
+        if(opr=='lw'):
+            rd=decoded_info[this_pc].get('rd','-1')
+            x=int(rd,2)
+            if(rz in data_dict):
+                mem_df_reg[x]=data_dict[rz]
+                val_df_reg[x]=mem_df_reg[x]
+
         muxy,data_dict,temp_string_memory=memory.memory(0x0,rz,[decoded_info[this_pc]['type'],decoded_info[this_pc]['opr']],rm,data_dict,pc_temp)
 
 
@@ -89,6 +113,11 @@ while(1):
         #print("nonoo")
         rz,pc_final,temp_string_execute=execute.execute(decoded_info[this_pc],reg,pc_temp)
         rz=hex(rz)
+        buffer_exec=rz
+        rd=decoded_info[this_pc].get('rd','-1')
+        if(rd!='-1'):
+            x=int(rd,2)            
+            val_df_reg[x]=rz
         if control_inst:
             control_inst=False
             if this_pc in btb:
@@ -120,6 +149,22 @@ while(1):
         instruction_register=instruction_dict[this_pc]
         pc_temp=fetch.increment_pc(this_pc)
         decoded_info[this_pc]=decode.decode(instruction_register)
+        rd=decoded_info[this_pc].get('rd','-1')
+        rs1=decoded_info[this_pc].get('rs1','-1')
+        rs2=decoded_info[this_pc].get('rs2','-1')
+        if(rs1!='-1'):
+            x=int(rs1,2)
+            if(write_df_reg[x]==1):#that means data forwarding is required
+                reg[x]=val_df_reg[x] # i add the value stored in the buffer
+        if(rs2!='-1'):
+            x=int(rs2,2)
+            if(write_df_reg[x]==1):#that means data forwarding is required
+                reg[x]=val_df_reg[x] # i add the value stored in the buffer          
+        
+        if(rd!='-1'):
+            x=int(rd,2)
+            write_df_reg[x]=1
+        print(this_pc,decoded_info[this_pc])
         opr=decoded_info[this_pc]['opr']
         if(opr=='jal' or opr=='jalr' or opr=='beq' or opr=='bne' or opr=='bge' or opr=='blt'):
             #print("cheee")
