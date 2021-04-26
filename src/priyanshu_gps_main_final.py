bdc8138f30de701f8b1978aa5b5ff4207c05e444
Gurkirpal Singh <sgurkirpal88@gmail.com>
2021-04-26 19:40:24 +0530

    Add files

--- src/priyanshu_gps_main_final.py
+++ src/priyanshu_gps_main_final.py
@@ -48,8 +48,8 @@ fetch_pc = []
 decode_pc.append("0x0")
 control_inst = False
 remove_decode = False
+
 while(1):
-    print("front",decode_pc, execute_pc, mem_pc, write_pc)
     if len(write_pc) == 0 and len(mem_pc) == 0 and len(execute_pc) == 0 and len(decode_pc) == 0:
         break
     clock += 1
@@ -58,13 +58,18 @@ while(1):
     if len(write_pc) != 0:
         this_pc = write_pc[0]
         write_pc.pop(0)
-        # print("write",this_pc)
+        print("write", this_pc)
         if('rd' in decoded_info[this_pc]):
             x = int(decoded_info[this_pc]['rd'], 2)
             write_df_reg[x] = write_df_reg[x]-1  # as now its use has ended
+            if(x == 0):
+                write_df_reg[x] = 0
+
             if(int(decoded_info[this_pc]['rd'], 2) != 0):
                 reg, temp_string_writeback = Writeback.write_back(
                     muxy, [decoded_info[this_pc]['type'], decoded_info[this_pc]['opr'], decoded_info[this_pc]['rd']], reg)
+        #print("write: ",decode_pc, execute_pc, mem_pc, write_pc)
+        print(reg)
 
     # memory
     if len(mem_pc) != 0:
@@ -89,13 +94,14 @@ while(1):
 
         muxy, data_dict, temp_string_memory = memory.memory(
             0x0, rz, [decoded_info[this_pc]['type'], decoded_info[this_pc]['opr']], rm, data_dict, pc_temp)
+        #print("mem: ",decode_pc, execute_pc, mem_pc, write_pc)
 
     # execute
     if len(execute_pc) != 0:
         this_pc = execute_pc[0]
         execute_pc.pop(0)
         mem_pc.append(this_pc)
-       # print("execute",this_pc)
+        # print("execute",this_pc)
         pc_temp = fetch.increment_pc(this_pc)
         # print("nonoo")
         rz, pc_final, temp_string_execute = execute.execute(
@@ -106,6 +112,8 @@ while(1):
         if(rd != '-1'):
             x = int(rd, 2)
             val_df_reg[x] = rz
+            if(x == 0):
+                val_df_reg[x] = 0
             if(decoded_info[this_pc]['opr'] == 'lw'):
                 val_df_reg[x] = data_dict[rz]
         if control_inst:
@@ -117,10 +125,8 @@ while(1):
                     remove_decode = True
                     if pc_final in instruction_dict:
                         decode_pc.append(pc_final)
-                        if btb[this_pc] in decoded_info:
-                            write_df_reg[int(decoded_info[btb[this_pc]]['rd'],2)]=0
-                        if decode_pc[0]==pc_final:
-                            remove_decode=False
+                        if decode_pc[0] == pc_final:
+                            remove_decode = False
             else:
                 btb[this_pc] = pc_final
                 if pc_final == fetch.increment_pc(this_pc):
@@ -129,56 +135,58 @@ while(1):
                     remove_decode = True
                     if pc_final in instruction_dict:
                         decode_pc.append(pc_final)
-                        if fetch.increment_pc(this_pc) in decoded_info: 
-                            write_df_reg[int(decoded_info[fetch.increment_pc(this_pc)]['rd'],2)]=0
-                        if decode_pc[0]==pc_final:
-                            remove_decode=False
+                        if decode_pc[0] == pc_final:
+                            remove_decode = False
+        #print("exec: ",decode_pc, execute_pc, mem_pc, write_pc)
 
     # decode
     if len(decode_pc) != 0:
         this_pc = decode_pc[0]
+        # print("decode",this_pc)
         decode_pc.pop(0)
-        instruction_register = instruction_dict[this_pc]
-        decoded_info[this_pc] = decode.decode(instruction_register)
+        flg = 0
         if remove_decode == False:
             # print("httt")
-            print(this_pc)
+            # print(this_pc)
             execute_pc.append(this_pc)
+            flg = 1
             if fetch.increment_pc(this_pc) in instruction_dict:
                 decode_pc.append(fetch.increment_pc(this_pc))
-        else:
-            remove_decode=False
-            continue
-        
         remove_decode = False
         # print("decode",this_pc)
-        
+        instruction_register = instruction_dict[this_pc]
 
         pc_temp = fetch.increment_pc(this_pc)
-        # print(decoded_info)
+        decoded_info[this_pc] = decode.decode(instruction_register)
+        # print(decoded_info[this_pc])
         # print(val_df_reg)
 
         rd = decoded_info[this_pc].get('rd', '-1')
         rs1 = decoded_info[this_pc].get('rs1', '-1')
         rs2 = decoded_info[this_pc].get('rs2', '-1')
-        if(rs1 != '-1'):
+        if(rs1 != '-1' and flg == 1):
             x = int(rs1, 2)
             if(write_df_reg[x] == 1):  # that means data forwarding is required
                 reg[x] = val_df_reg[x]  # i add the value stored in the buffer
-        if(rs2 != '-1'):
+        if(rs2 != '-1' and flg == 1):
             x = int(rs2, 2)
             if(write_df_reg[x] == 1):  # that means data forwarding is required
                 reg[x] = val_df_reg[x]  # i add the value stored in the buffer
 
-        if(rd != '-1'):
+        if(rd != '-1' and flg == 1):
             x = int(rd, 2)
-            write_df_reg[x] = write_df_reg[x]+1
+            if(x != 0):
+                write_df_reg[x] = write_df_reg[x]+1
+            else:
+                write_df_reg[x] = 0
+
         # print(write_df_reg)
         opr = decoded_info[this_pc]['opr']
         if(opr == 'jal' or opr == 'jalr' or opr == 'beq' or opr == 'bne' or opr == 'bge' or opr == 'blt'):
             # print("cheee")
             control_inst = True
-            if len(decode_pc)!=0:
+            if len(decode_pc) != 0:
+
                 decode_pc.pop()
             if this_pc in btb:
                 if btb[this_pc] in instruction_dict:
@@ -186,6 +194,7 @@ while(1):
             else:
                 if fetch.increment_pc(this_pc) in instruction_dict:
                     decode_pc.append(fetch.increment_pc(this_pc))
+
     # print(remove_decode)
     print(decode_pc, execute_pc, mem_pc, write_pc)
 
